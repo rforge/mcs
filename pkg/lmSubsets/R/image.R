08a8562e720d3a8516f61e3c00925ba92997b6ed
mhofmann <mhofmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-28 15:37:05 +0000

    enhanced 'image.lmSubsets' method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mcs@151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lmSubsets/R/image.R
+++ pkg/lmSubsets/R/image.R
@@ -1,69 +1,91 @@
 image.lmSubsets <- function(x, best = 1, size = NULL, which = NULL,
-  xlab = "", ylab = NULL, main = "Subset selection",
-  xaxs = "i", yaxs = "i", cex = 0.9,
-  col = gray.colors(2), rcol = 2, rect = "BIC", uline = rect,
-  srt = 45, ...)
-{
-  ## all subset selections
-  z <- x$which[, best, ]
-  if(!is.null(which)) z <- z[which, , drop = FALSE]
-  if(!is.null(size)) z <- z[, as.character(size), drop = FALSE]
-  z <- z[ , apply(!is.na(z), 2L, all), drop = FALSE]
-
-  ## empty plot
-  nr <- nrow(z)
-  nc <- ncol(z)
-  if(is.null(ylab)) ylab <- sprintf("Number of regressors (best = %s)", best)
-  plot(0, 0, xlim = c(0, nr) + 0.5, ylim = c(nc, 0) + 0.5, type = "n",
-    axes = FALSE, xaxs = xaxs, yaxs = yaxs,
-    xlab = xlab, ylab = ylab, main = main, ...)
-  
-  ## rectangles highlighting selection
-  graphics::rect(row(z) - 0.5, col(z) - 0.5, row(z) + 0.5, col(z) + 0.5,
-    col = col[2L - z], border = NA)
-
-  ## highlight particular selection(s)
-  if(!is.null(rect) | !identical(rect, FALSE)) {
-    if(identical(rect, TRUE)) rect <- "BIC"    
-    if(is.character(rect)) {
-      rect <- summary(x, penalty = rect)$summary$val[best, ]
-      rect <- as.numeric(names(rect))[which.min(rect)]
+                            main = "Subset selection", xlab = "", ylab = NULL,
+                            xaxs = "i", yaxs = "i", cex = 0.9,
+                            col = gray.colors(2), hilite = "BIC", hilite.hue = 0,
+                            uline = hilite, srt = 45, gap = "white", ...) {
+    hilite.colors <- function (n, hue, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL) {
+        ans <- seq.int(from = start^gamma, to = end^gamma, length.out = n)^(1/gamma)
+        ans <- hsv(hue, ans, 1, alpha)
+        rev(ans)
     }
-    y_rect <- match(rect, as.numeric(colnames(z)))
-    if(all(!is.na(y_rect))) {
-      graphics::rect(rep(0.5, length(y_rect)), y_rect - 0.5,
-        rep(nr, length(y_rect)) + 0.5, y_rect + 0.5, border = rcol)
-    }
-  } else {
-    rect <- NULL
-  }
 
-  ## x-axis (variable) labels
-  rlab <- rownames(z)
-  if(!is.null(uline) | !identical(uline, FALSE)) {
-    if(identical(uline, TRUE)) uline <- "BIC"    
-    if(is.character(uline)) {
-      uline <- summary(x, penalty = uline)$summary$val[best, ]
-      uline <- as.numeric(names(uline))[which.min(uline)]
-    }
-    y_uline <- match(uline, as.numeric(colnames(z)))
-    if(all(!is.na(y_uline))) {
-      y_uline <- which(apply(z[, y_uline, drop = FALSE], 1L, any))
-      rlab <- sapply(seq_along(rlab), function(i)
-        if(i %in% y_uline) parse(text = paste("underline(", rlab[i], ")", sep = "")) else parse(text = rlab[i]))
-    }
-  } else {
-    uline <- NULL
-  }
-  text(1L:nr, par("usr")[3] + 0.02 * nc, labels = rlab, srt = srt, 
-    adj = c(1, 1), xpd = TRUE, cex = cex)  
+    ## which
+    arg.which <- which;  which <- NULL
+    which <- x$which[, best, ]
+    if (!is.null(arg.which))  which <- which[arg.which, , drop = FALSE]
+    if (!is.null(size))  which <- which[, as.character(size), drop = FALSE]
+    which <- which[ , apply(!is.na(which), 2L, all), drop = FALSE]
+    
+    M <- nrow(which)
+    N <- ncol(which)
 
-  ## y-axis (subset) labels
-  clab0 <- as.numeric(colnames(z))
-  clab <- pretty(clab0)
-  clab[1L] <- pmax(clab[1L], clab0[1L])
-  clab[length(clab)] <- pmin(clab[length(clab)], clab0[length(clab0)])
-  clab <- sort(unique(c(clab, uline, rect)))
-  axis(2, at = match(clab, clab0), labels = clab)
-  box()
+    ## heatmap
+    heatmap <- structure(col[2L - which], dim = c(M, N))
+
+    ## highlight
+    if (!is.null(hilite) || !identical(hilite, FALSE)) {
+        if (identical(hilite, TRUE))  hilite <- "BIC"    
+        if (is.character(hilite)) {
+            hilite <- summary(x, penalty = hilite)$summary$val[best, ]
+            hilite <- as.numeric(names(hilite))[which.min(hilite)]
+        }
+        y_hilite <- match(hilite, as.numeric(colnames(which)))
+        if (!all(is.na(y_hilite))) {
+            hilite.hue <- rep(hilite.hue, length.out = length(y_hilite))
+            hilite.hue <- hilite.hue[!is.na(y_hilite)]
+            y_hilite <- y_hilite[!is.na(y_hilite)]
+            for (i in seq_along(y_hilite)) {
+                heatmap[, y_hilite[i]] <- hilite.colors(2, hilite.hue[i])[2L - which[, y_hilite[i], drop = FALSE]]
+            }
+        }
+    } else {
+        hilite <- NULL
+    }
+
+    ## underline
+    rlab <- rownames(which)
+    if (!is.null(uline) || !identical(uline, FALSE)) {
+        if (identical(uline, TRUE))  uline <- "BIC"    
+        if (is.character(uline)) {
+            uline <- summary(x, penalty = uline)$summary$val[best, ]
+            uline <- as.numeric(names(uline))[which.min(uline)]
+        }
+        y_uline <- match(uline, as.numeric(colnames(which)))
+        if (!all(is.na(y_uline))) {
+            y_uline <- which(apply(which[, y_uline, drop = FALSE], 1L, any))
+            rlab <- sapply(seq_along(rlab), function (i) {
+                if(i %in% y_uline)
+                    parse(text = paste("underline(", rlab[i], ")", sep = ""))
+                else
+                    parse(text = rlab[i])
+            })
+        }
+    } else {
+        uline <- NULL
+    }
+
+    ## empty plot
+    if (is.null(ylab))  ylab <- sprintf("Number of regressors (best = %s)", best)
+    plot(0, 0, xlim = c(0, M) + 0.5, ylim = c(N, 0) + 0.5, type = "n",
+         axes = FALSE, xaxs = xaxs, yaxs = yaxs, xlab = xlab, ylab = ylab,
+         main = main, ...)
+    
+    ## paint heatmap
+    graphics::rect(row(which) - 0.5, col(which) - 0.5, row(which) + 0.5, col(which) + 0.5,
+                   col = heatmap, border = gap)
+
+    ## x-axis labels (variable names)
+    text(1L:M, par("usr")[3] + 0.02 * N, labels = rlab, srt = srt, 
+         adj = c(1, 1), xpd = TRUE, cex = cex)  
+
+    ## y-axis labels (subset sizes)
+    clab0 <- as.numeric(colnames(which))
+    clab <- pretty(clab0)
+    clab[1L] <- pmax(clab[1L], clab0[1L])
+    clab[length(clab)] <- pmin(clab[length(clab)], clab0[length(clab0)])
+    clab <- sort(unique(c(clab, uline, hilite)))
+    axis(2, at = match(clab, clab0), labels = clab)
+
+    ## box
+    box()
 }
